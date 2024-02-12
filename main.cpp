1f7afcd20e40575b7189bcc747719ca87c5248a5
EkaterinaMiller <evmiller@nic.edu>
2024-02-12 09:21:23 -0800

    Addid a for loop

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ int main()
     cout<<"Hello World!"<<endl;
     name();
     cout<<"My name is Ekaterina\n";
+    for (int i=0; i<5; i++){
+        cout<<"Cat\n";
+    }
     return 0;
 }
 void name(){
