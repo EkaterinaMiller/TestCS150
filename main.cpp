74ed9ebf1e48247a80c2eaf0235264cfd1a9e95a
EkaterinaMiller <43764013+EkaterinaMiller@users.noreply.github.com>
2024-02-12 17:20:56 +0000

    Addid while loop

--- main.cpp
+++ main.cpp
@@ -7,6 +7,12 @@ int main()
     cout<<"Hello World!"<<endl;
     name();
     cout<<"My name is Ekaterina\n";
+    int i=0;
+    while ( i<5)
+    {
+        cout<<"Cat\n";
+        i++;
+    }
     return 0;
 }
 void name(){
