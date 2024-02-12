b4513faa467758dd7d59b3d4ee3beec7aba9083f
EkaterinaMiller <43764013+EkaterinaMiller@users.noreply.github.com>
2024-02-12 17:12:38 +0000

    Add void name()

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 using namespace std;
 
+void name();
 int main()
 {
     cout<<"Hello World!"<<endl;
+    name();
     return 0;
+}
+void name(){
+    cout<< "My name is Ekaterina\n";
 }
